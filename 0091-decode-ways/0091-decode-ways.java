107376ce25c635e8c220d8da4a345868e7c19d21
Vishesh Pandita <39872739+VisheshPandita@users.noreply.github.com>
2024-11-20 06:03:44 +0530

    Time: 1 ms (80.31%), Space: 41.2 MB (98.42%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,25 +1,19 @@
 class Solution {
-    private int dfs(int i, String s, Map<Integer, Integer> dp) {
-        if(dp.containsKey(i)) {
-            return dp.get(i);
-        }
-        if(i==s.length()) return 1;
-        if(s.charAt(i)=='0') return 0;
-
-        int res = dfs(i+1, s, dp);
-        if(i<s.length()-1){
-            if (s.charAt(i) == '1' || 
-               (s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
-                res += dfs(i + 2, s, dp);
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()+1];
+        dp[s.length()] = 1;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='0'){
+                dp[i]=0;
+            } else {
+                dp[i] = dp[i+1];
+                if (i + 1 < s.length() && (s.charAt(i) == '1' || 
+                    s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
             }
         }
-        dp.put(i, res);
-        return res;
-    }
 
-    public int numDecodings(String s) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        dp.put(s.length(), 1);
-        return dfs(0, s, dp);
+        return dp[0];
     }
 }
