cb599ed330b44da249819dc13572de6b93477500
Vishesh Pandita <39872739+VisheshPandita@users.noreply.github.com>
2025-01-02 21:39:29 +0530

    Time: 5 ms (78.03%), Space: 46.2 MB (21.56%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> prereq = new HashMap<>();
-        for(int[] pair: prerequisites){
-            prereq.computeIfAbsent(pair[0], k->new ArrayList<>()).add(pair[1]);
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++) {
+            graph.add(new ArrayList<>());
         }
 
-        List<Integer> output = new ArrayList<>();
-        Set<Integer> visit = new HashSet<>();
-        Set<Integer> cycle = new HashSet<>();
+        for(int[] prereq: prerequisites) {
+            int course = prereq[0];
+            int prerequisite = prereq[1];
+            graph.get(prerequisite).add(course);
+            indegree[course]++;
+        }
 
-        for(int course=0;course<numCourses;course++){
-            if(!dfs(course, prereq, visit, cycle, output)){
-                return new int[0];
-            }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) queue.offer(i);
         }
 
         int[] result = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            result[i] = output.get(i);
+        int ptr = 0;
+        while(!queue.isEmpty()){
+            int course = queue.poll();
+            result[ptr] = course;
+            ptr++;
+
+            for(int neighbour: graph.get(course)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    queue.offer(neighbour);
+                }
+            }
         }
 
-        return result;
-    }
-
-    private boolean dfs(int course, Map<Integer, List<Integer>> prereq, Set<Integer> visit, Set<Integer> cycle, List<Integer> output) {
-        if(cycle.contains(course)) return false;
-        if(visit.contains(course)) return true;
-
-        cycle.add(course);
-        for(int pre: prereq.getOrDefault(course, Collections.emptyList())){
-            if(!dfs(pre, prereq, visit, cycle, output)) return true;
-        }
-        cycle.remove(course);
-        visit.add(course);
-        output.add(course);
-        return true;
+        return (ptr == numCourses) ? result : new int[]{};
     }
 }
