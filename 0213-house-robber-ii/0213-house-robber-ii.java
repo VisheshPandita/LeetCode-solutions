ba74ce6d4e3edab6cab9fc954084e92523558ecd
Vishesh Pandita <39872739+VisheshPandita@users.noreply.github.com>
2026-06-28 17:27:40 +0530

    Time: 0 ms (100%), Space: 43.1 MB (7.84%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,17 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        return Math.max(helper(Arrays.copyOfRange(nums, 1, nums.length)), helper(Arrays.copyOfRange(nums, 0, nums.length-1)));
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        int maxA = robRange(nums, 0, nums.length-2);
+        int maxB = robRange(nums, 1, nums.length-1);
+
+        return Math.max(maxA, maxB);
     }
 
-    private int helper(int[] nums){
-        int rob1 = 0, rob2 = 0;
-        for(int num: nums) {
-            int temp = Math.max(num+rob1, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+    private int robRange(int[] nums, int start, int end) {
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=start;i<=end;i++){
+            int currentMax = Math.max(prev1, prev2+nums[i]);
+
+            prev2 = prev1;
+            prev1 = currentMax;
         }
 
-        return rob2;
+        return prev1;
     }
 }
